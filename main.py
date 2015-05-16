e3be38d6f00d2d60f41033ac98e78eee2b01721b
erwan <erwan@malangot.fr>
2015-05-15 23:35:39 -0400

    testing

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from cpasbien_module import *
+from cpasbien_variables import *
+import transmissionrpc
+
+
+# TODO : Remplacer urllib par request
+
+# Création de l'objet transmission
+OBJ_transmission = transmissionrpc.Client(address = ADRESS_RPC, port = PORT_RPC, user = LOGIN_RPC, password = PASSWORD_RPC)
+
+# Boucle par catégorie
+for categorie in listeCategorie:
+    historique = chargement_historique(categorie)
+    # Boucle par page de téléchargement
+    for urlPageDl in extraction_liens_pages(categorie):
+        # TODO : Etape de vérification de la présence
+        lienTorrent = extraction_liens_torrent(urlPageDl)
+        if verif_pour_telechargement(lienTorrent, historique):
+            if categorie == 'films':
+                if 'vostfr' in lienTorrent.split('-'):
+                    telechargement_torrent(OBJ_transmission, lienTorrent, DIR_FILMS + "VOSTFR")
+                    sauvegarde_historique(categorie, lienTorrent)
+                elif 'french' in lienTorrent.split('-'):
+                    telechargement_torrent(OBJ_transmission, lienTorrent, DIR_FILMS + "FR")
+                    sauvegarde_historique(categorie, lienTorrent)
+                else:
+                    telechargement_torrent(OBJ_transmission, lienTorrent, DIR_DEFAULT)
+                    sauvegarde_historique(categorie, lienTorrent)
+            if categorie == 'series':
+                if 'vostfr' in lienTorrent.split('-'):
+                    telechargement_torrent(OBJ_transmission, lienTorrent, DIR_SERIES + "VOSTFR")
+                    sauvegarde_historique(categorie, lienTorrent)
+                elif 'french' in lienTorrent.split('-'):
+                    telechargement_torrent(OBJ_transmission, lienTorrent, DIR_SERIES + "FR")
+                    sauvegarde_historique(categorie, lienTorrent)
+                else:
+                    telechargement_torrent(OBJ_transmission, lienTorrent, DIR_DEFAULT)
+                    sauvegarde_historique(categorie, lienTorrent)
+            if categorie == 'musique':
+                telechargement_torrent(OBJ_transmission, lienTorrent, DIR_MUSIQUE)
+                sauvegarde_historique(categorie, lienTorrent)

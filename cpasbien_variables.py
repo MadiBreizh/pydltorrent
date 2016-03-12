b9a4432bac01865eb5b7b3b83cc69d4304bbef61
erwan <erwan@malangot.fr>
2016-03-12 00:28:53 -0400

    modification divers

---
+++ cpasbien_variables.py
@@ -0,0 +1,11 @@
+ADRESS_RPC = '127.0.0.1'
+PORT_RPC = '9091'
+LOGIN_RPC = ''
+PASSWORD_RPC = ''
+
+DIR_FILMS = ""
+DIR_SERIES = ""
+DIR_MUSIQUE = ""
+DIR_DEFAULT = ""
+
+listeCategorie = ['films', 'series', 'musique']
